5ac70b1da50f5c7b930cda33dc838bebe8795246
like-rounak <rounaks.it.ug@jadavpuruniversity.in>
2023-08-28 20:44:46 +0530

    v2.0

--- script.js
+++ script.js
@@ -1,51 +1,104 @@
-const conferenceContainer = document.getElementById('conferenceContainer');
+// Fetch and display conference data from the API
+async function fetchConferences() {
+  const response = await fetch("https://gdscdev.vercel.app/api");
+  const data = await response.json();
+  const conferences = data.content.data;
 
-fetch('https://gdscdev.vercel.app/api')
-  .then(response => response.json())
-  .then(data => {
-    const conferences = data.content.data;
+  const conferenceContainer = document.getElementById("conferenceContainer");
+  conferenceContainer.innerHTML = "";
 
-    conferences.forEach(conference => {
-      const conferenceCard = document.createElement('div');
-      conferenceCard.className = 'conference-card';
+  conferences.forEach(conference => {
+    const conferenceCard = createConferenceCard(conference);
+    conferenceContainer.appendChild(conferenceCard);
+  });
+}
 
-      const bannerImage = document.createElement('img');
-      bannerImage.src = conference.banner_image;
-      bannerImage.alt = conference.title;
+// Create a conference card element
+function createConferenceCard(conference) {
+  const conferenceCard = document.createElement("div");
+  conferenceCard.className = "conference-card";
 
-      const titleElement = document.createElement('h2');
-      titleElement.textContent = conference.title;
+  const image = document.createElement("img");
+  image.src = conference.banner_image;
+  image.alt = conference.title;
+  conferenceCard.appendChild(image);
 
-      const descriptionElement = document.createElement('p');
-      descriptionElement.textContent = conference.description;
+  const title = document.createElement("h2");
+  title.textContent = conference.title;
+  conferenceCard.appendChild(title);
 
-      const dateTimeElement = document.createElement('p');
-      const dateTime = new Date(conference.date_time).toLocaleString();
-      dateTimeElement.textContent = `Date and Time: ${dateTime}`;
+  const description = document.createElement("p");
+  description.textContent = conference.description;
+  conferenceCard.appendChild(description);
 
-      const organiserElement = document.createElement('p');
-      organiserElement.textContent = `Organizer: ${conference.organiser_name}`;
+  const organiserIcon = document.createElement("img");
+  organiserIcon.className = "organiser-icon";
+  organiserIcon.src = conference.organiser_icon;
+  organiserIcon.alt = conference.organiser_name;
+  conferenceCard.appendChild(organiserIcon);
 
-      const organiserIcon = document.createElement('img');
-      organiserIcon.src = conference.organiser_icon;
-      organiserIcon.alt = `${conference.organiser_name} Icon`;
-      organiserIcon.className = 'organiser-icon'; // Make sure class name matches CSS
+  const organiserName = document.createElement("span");
+  organiserName.textContent = conference.organiser_name;
+  conferenceCard.appendChild(organiserName);
 
-      const venueElement = document.createElement('p');
-      venueElement.textContent = `Venue: ${conference.venue_name}, ${conference.venue_city}, ${conference.venue_country}`;
+  const venue = document.createElement("p");
+  venue.className = "venue-country";
+  venue.textContent = `${conference.venue_name}, ${conference.venue_city}, ${conference.venue_country}`;
+  conferenceCard.appendChild(venue);
 
-      conferenceCard.appendChild(bannerImage);
-      conferenceCard.appendChild(titleElement);
-      conferenceCard.appendChild(descriptionElement);
-      conferenceCard.appendChild(dateTimeElement);
+  const dateTimeContainer = document.createElement("div");
+  dateTimeContainer.className = "date-time-container";
 
-      // Append the organizer icon before the organizer's name
-      organiserElement.prepend(organiserIcon);
+  const date = document.createElement("p");
+  const dateTime = new Date(conference.date_time);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  date.textContent = dateTime.toLocaleDateString(undefined, options);
+  dateTimeContainer.appendChild(date);
 
-      conferenceCard.appendChild(organiserElement);
-      conferenceCard.appendChild(venueElement);
+  const time = document.createElement("p");
+  const timeString = dateTime.toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit' });
+  time.textContent = `Time: ${timeString}`;
+  dateTimeContainer.appendChild(time);
 
-      conferenceContainer.appendChild(conferenceCard);
-    });
-  })
-  .catch(error => console.error('Error fetching data:', error));
+  conferenceCard.appendChild(dateTimeContainer);
+
+  return conferenceCard;
+}
+
+// Call fetchConferences to load data initially
+fetchConferences();
+
+// Filtering functionality
+document.getElementById("filterBy").addEventListener("change", function() {
+  const filterBy = this.value.toLowerCase();
+  const conferenceContainer = document.getElementById("conferenceContainer");
+
+  const conferenceCards = Array.from(conferenceContainer.children);
+  conferenceCards.forEach(card => {
+    const country = card.querySelector(".venue-country").textContent.toLowerCase();
+
+    if (filterBy === "all" || country.includes(filterBy)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
+
+// Searching functionality
+document.getElementById("searchBox").addEventListener("input", function() {
+  const searchTerm = this.value.toLowerCase();
+  const conferenceContainer = document.getElementById("conferenceContainer");
+
+  const conferenceCards = Array.from(conferenceContainer.children);
+  conferenceCards.forEach(card => {
+    const title = card.querySelector(".conference-card h2").textContent.toLowerCase();
+    const description = card.querySelector(".conference-card p").textContent.toLowerCase();
+
+    if (title.includes(searchTerm) || description.includes(searchTerm)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
